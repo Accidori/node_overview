dee327895f096727bc59801f2ef44249c53ebc0e
Funkyskep <phyrowolf24@gmail.com>
2024-10-01 13:02:30 -0400

    Array methods

--- index.js
+++ index.js
@@ -1,39 +1,149 @@
-const mathTools ={
-    num1: 15,
-    num2: 10,
-    sum: function(){
-        const nested = () =>{
-            console.log('nested', this.num1);
-            const moreNested = () =>{
-                console.log('moreNested', this.num2);
-            }
-            moreNested();
-        }
-        
-        nested();
+const nums = [10, 3, 100, 70, 8, 55, 88]
+const users=[
+    {
+        name: 'Bob',
+        age: 99
 
-        return this.num1 + this.num2;
     },
-    difference: function(){
-        return this.num1 - this.num2;
-    } 
-}
+    {
+        name: 'Jim',
+        age: 25
+    },
+    {
+        name: 'Sarah',
+        age: 40
 
-const sum = mathTools.sum();
-const difference = mathTools.difference();
-
-console.log('sum', sum)
-console.log('difference', difference)
+    }
+]
 
 
+//strudents
+const students=[
+    {
+        name: 'Me',
+        average: 99
 
-// function printName(userName){
-//     const capitalize = str => str[0].toUpperCase() + str.slice(1);
-//     const capName = capitalize(userName);
-//     console.log(capName);
+    },
+    {
+        name: 'You',
+        average: 90
+    },
+    {
+        name: 'Them',
+        average: 80
+
+    },
+    {
+        name: 'Us',
+        average: 70
+    },
+    {
+        name: 'We',
+        average: 85
+
+    }
+]
+
+const sum = nums.reduce((output, num) => {
+    return output + num
+
+}, 0)
+
+console.log(sum);
+
+const overallAverage = students.reduce((output, studentObj, index, arr) => {
+    if (index === arr.length -1){
+        return (output + studentObj.average) / arr.length
+    }
+    
+    
+    
+    return output + studentObj.average
+
+}, 0)
+
+let total= 0;
+
+
+
+// for (const studentObj of students) {
+//     total +=
+    
 // }
 
-// printName('bob');
 
-// mathTools.sum;
+console.log(overallAverage);
 
+
+
+
+
+// //checking if even
+// const evenArray = nums.map((num) => {
+//     if ((num % 2) === 0){
+//         return `${num} is even`
+            
+//     }else{
+//         return `${num} is odd`
+//     }
+// });
+
+// console.log(evenArray);
+
+
+// //Filter Numbers
+// const filteredNums = nums.filter((num) => {
+//     if (num > 10){
+//         return true;
+//     }
+// });
+
+// console.log(filteredNums);
+
+
+// //Filters Users
+// const filteredUsers = users.filter((users) => {
+//     if (users > 10){
+//         return true;
+//     }
+// });
+
+// console.log(filteredUsers);
+
+
+
+
+
+// //Sort Numbers
+// const sorted = nums.sort((currentNum, nextNum) => {
+//     return nextNum - currentNum
+// });
+
+// console.log(sorted);
+
+
+// //Sort Users
+// const sortedUsers = users.sort((currentObj, nextObj) => {
+//     return currentObj.age - nextObj.age;
+// });
+
+// console.log(sortedUsers);
+
+
+
+
+
+/*METHOD/HIGH-END FUNCTIONS EXAMPLES*/
+
+// nums.forEach((num, index) => {
+//     console.log(index);
+// });;
+
+
+// for (const num of nums){
+//     if (num > 10){
+//         break;
+//     }
+// console.log(num);
+
+// }
