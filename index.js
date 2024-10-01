3bb3ee94d9b6cd582cb10922f89d39b823a04e34
Funkyskep <phyrowolf24@gmail.com>
2024-10-01 13:53:59 -0400

    spreaded

--- index.js
+++ index.js
@@ -1,149 +1,38 @@
-const nums = [10, 3, 100, 70, 8, 55, 88]
-const users=[
-    {
-        name: 'Bob',
-        age: 99
+// const fruits = ['orange','apple','grape']
+// const fruits2 = ['kiwi','banana','strawberry']
 
-    },
-    {
-        name: 'Jim',
-        age: 25
-    },
-    {
-        name: 'Sarah',
-        age: 40
 
+//the ... is the spread operator which tkaes all of the arguments in an array
+
+// // const combined = [...fruits, ...fruits2];
+
+const data ={
+    name:'Me',
+    age: 44
+}
+
+const data2 ={
+    name:'You',
+    age: 99
+}
+
+const combinedData = {
+    ...data,
+    address: 'somewhere',
+    number: '999-999-9999'
+};
+
+console.log(combinedData);
+
+
+
+
+function printNames(...names){
+    for (const name of names){
+        console.log(name);
     }
-]
 
+}
 
-//strudents
-const students=[
-    {
-        name: 'Me',
-        average: 99
-
-    },
-    {
-        name: 'You',
-        average: 90
-    },
-    {
-        name: 'Them',
-        average: 80
-
-    },
-    {
-        name: 'Us',
-        average: 70
-    },
-    {
-        name: 'We',
-        average: 85
-
-    }
-]
-
-const sum = nums.reduce((output, num) => {
-    return output + num
-
-}, 0)
-
-console.log(sum);
-
-const overallAverage = students.reduce((output, studentObj, index, arr) => {
-    if (index === arr.length -1){
-        return (output + studentObj.average) / arr.length
-    }
-    
-    
-    
-    return output + studentObj.average
-
-}, 0)
-
-let total= 0;
-
-
-
-// for (const studentObj of students) {
-//     total +=
-    
-// }
-
-
-console.log(overallAverage);
-
-
-
-
-
-// //checking if even
-// const evenArray = nums.map((num) => {
-//     if ((num % 2) === 0){
-//         return `${num} is even`
-            
-//     }else{
-//         return `${num} is odd`
-//     }
-// });
-
-// console.log(evenArray);
-
-
-// //Filter Numbers
-// const filteredNums = nums.filter((num) => {
-//     if (num > 10){
-//         return true;
-//     }
-// });
-
-// console.log(filteredNums);
-
-
-// //Filters Users
-// const filteredUsers = users.filter((users) => {
-//     if (users > 10){
-//         return true;
-//     }
-// });
-
-// console.log(filteredUsers);
-
-
-
-
-
-// //Sort Numbers
-// const sorted = nums.sort((currentNum, nextNum) => {
-//     return nextNum - currentNum
-// });
-
-// console.log(sorted);
-
-
-// //Sort Users
-// const sortedUsers = users.sort((currentObj, nextObj) => {
-//     return currentObj.age - nextObj.age;
-// });
-
-// console.log(sortedUsers);
-
-
-
-
-
-/*METHOD/HIGH-END FUNCTIONS EXAMPLES*/
-
-// nums.forEach((num, index) => {
-//     console.log(index);
-// });;
-
-
-// for (const num of nums){
-//     if (num > 10){
-//         break;
-//     }
-// console.log(num);
-
-// }
+printNames('bob', 'frank', 'jill', 'billy', 'jane');
+printNames('jake', 'john');
